c9407e0b3119c6fba6ac87edad98ac5ee309e9f1
FORGE-JITA\ktokt <ulfnew@gmail.com>
2020-11-12 21:38:19 +0500

    Write code and message of processing result

--- ddl.sql
+++ ddl.sql
@@ -1,22 +1,28 @@
 alter table receive_table
     add is_processed boolean default false;
-
 comment on column receive_table.is_processed
     is 'Флаг сообщение обработано';
 
 create index receive_table_is_processed_created_at_index
     on receive_table (is_processed, created_at);
 
-CREATE TABLE if not exists message_attachment
+CREATE TABLE if not exists smev_receive_message_attachment
 (
-    id         bigserial not null
-        constraint message_attachment_pk
+    id                bigserial not null
+        constraint smev_receive_message_attachment_pk
             primary key,
-    request_id text      not null,
-    detail     jsonb     not null
+    receive_table_uid bigint    not null
+        constraint
+            smev_receive_message_attachment_receive_table_uid_fk
+            references receive_table,
+    filename          varchar   not null,
+    detail            jsonb     not null,
+    created_at        timestamp(0) default CURRENT_TIMESTAMP,
+    updated_at        timestamp(0)
 );
-create unique index message_attachment_request_id_uindex
-    on message_attachment (request_id);
+create unique index smev_message_attachment_request_id_uindex
+    on public.smev_receive_message_attachment
+        (receive_table_uid, filename);
 
 
 
--- parser.php
+++ parser.php
@@ -1,5 +1,10 @@
 <?php
 
+const CODE_403_REJECT = 403;
+const CODE_500_FAIL = 500;
+const CODE_200_OK = 200;
+const MESSAGE_200_OK = 'OK';
+
 $host = getenv('DB_HOST');
 $baseName = getenv('DB_NAME');
 $login = getenv('DB_USER');
@@ -18,19 +23,22 @@ if ($unitOfWorkSize === false) {
     $unitOfWorkSize = 100;
 }
 
-const OBTAIN_CONTENT = "
-select uid, content, error_code, error_text
+const OBTAIN_CONTENT = '
+select uid, content
 from receive_table 
 where is_processed = false
 order by is_processed, created_at
 limit :unit_of_work_size
 for update
-";
+';
 
 const MARK_AS_PROCESSED = '
 update receive_table
-set is_processed = true
-where uid = :uid
+set is_processed = true,
+    error_code = :result_code,
+    error_text = :result_message,
+    updated_at = CURRENT_TIMESTAMP
+where is_processed = false and uid = :uid
 ';
 
 const DEFINE_MESSAGE_IDENTITY = '
@@ -38,7 +46,7 @@ update receive_table
 set request_id = :request_id,
     message_type = :message_type,
     updated_at = CURRENT_TIMESTAMP
-where uid = :uid
+where is_processed = false and uid = :uid
 ';
 
 const SET_MESSAGE_STATUS = '
@@ -50,9 +58,9 @@ where id = :id
 ';
 
 const WRITE_ATTACHMENT = '
-insert into message_attachment 
-    (request_id, detail)
-    VALUES (:request_id,:detail)
+insert into smev_receive_message_attachment 
+    (receive_table_uid, filename,detail,created_at)
+    VALUES (:uid,:filename,:detail,CURRENT_TIMESTAMP)
 ';
 
 const WRITE_LICENSE = '
@@ -89,13 +97,21 @@ if ($defineIdentity !== false) {
     $defineIdentity->bindParam(':uid', $uid);
 }
 
+$resultCode = CODE_200_OK;
+$resultMessage = MESSAGE_200_OK;
 $markAsProcessed = $db->prepare(MARK_AS_PROCESSED);
 if ($markAsProcessed !== false) {
+    $markAsProcessed->bindParam(
+        ':result_code',
+        $resultCode,
+        PDO::PARAM_INT
+    );
+    $markAsProcessed->bindParam(':result_message', $resultMessage);
     $markAsProcessed->bindParam(':uid', $uid);
 }
 
 $setStatus = $db->prepare(SET_MESSAGE_STATUS);
-$errorCode = '';
+$errorCode = 0;
 $errorText = '';
 if ($setStatus !== false) {
     $setStatus->bindParam(':error_code', $errorCode);
@@ -105,8 +121,10 @@ if ($setStatus !== false) {
 
 $writeAttachment = $db->prepare(WRITE_ATTACHMENT);
 $detailJson = '';
+$archiveName = '';
 if ($writeAttachment !== false) {
-    $writeAttachment->bindParam(':request_id', $replyToClientId);
+    $writeAttachment->bindParam(':uid', $uid);
+    $writeAttachment->bindParam(':filename', $archiveName);
     $writeAttachment->bindParam(':detail', $detailJson);
 }
 $writeLicense = $db->prepare(WRITE_LICENSE);
@@ -124,114 +142,158 @@ if ($cursor !== false && $markAsProcessed !== false) {
 while ($cursor && $record = $cursor->fetch(PDO::FETCH_ASSOC)) {
     $content = $record['content'];
 
-    $reader = (new XMLReader());
-    $reader->XML($content);
+    try {
+        $reader = (new XMLReader());
+        $reader->XML($content);
 
-    $messageType = '';
-    $replyToClientId = '';
-    $catchId = false;
-    $isStatus = false;
-    $isPrimary = false;
-    $isFile = false;
-    $isLicense = false;
-    $filename = '';
-    $fileType = '';
-    $archiveName = '';
-    $detailJson = '';
-    while ($reader->read()) {
-        if ($isStatus && $catchId) {
-            break;
+        $messageType = '';
+        $replyToClientId = '';
+        $catchId = false;
+        $isStatus = false;
+        $isPrimary = false;
+        $isFile = false;
+        $isLicense = false;
+        $filename = '';
+        $fileType = '';
+        $archiveName = '';
+        $detailJson = '';
+        $messageId = '';
+        $catchMessageId = false;
+        $isReject = false;
+        $errorCode = 0;
+        $errorText = '';
+        $catchCode = false;
+        $catchText = false;
+        $catchFilename = true;
+        $catchFileType = true;
+        $catchArchiveName = true;
+        $statusCode = '';
+        while ($reader->read()) {
+            if ($isStatus && $catchId && $catchCode && $catchText) {
+                break;
+            }
+            if ($isFile
+                && $catchId
+                && $catchFilename
+                && $catchFileType
+                && $catchArchiveName
+                && $catchMessageId
+            ) {
+                break;
+            }
+            if ($isLicense && $catchId) {
+                break;
+            }
+
+            $name = $reader->name;
+            $isElement = $reader->nodeType === XMLReader::ELEMENT;
+
+            if ($name === 'MessageId' && $isElement) {
+                $messageId = $reader->expand()->textContent;
+                $catchMessageId = true;
+            }
+            if ($name === 'messageType' && $isElement) {
+                $messageType = $reader->expand()->textContent;
+                $isStatus = $messageType === 'StatusMessage'
+                    || $messageType === 'RejectMessage';
+                $isPrimary = $messageType === 'PrimaryMessage';
+            }
+            if ($isStatus
+                && $name === 'code'
+                && $isElement) {
+                $statusCode = $reader->expand()->textContent;
+                $errorCode = (int)$statusCode;
+
+                $catchCode = true;
+            }
+            if ($isStatus
+                && $name === 'description'
+                && $isElement) {
+                $errorText = $reader->expand()->textContent;
+                $catchText = true;
+            }
+            if ($name === 'replyToClientId' && $isElement) {
+                $replyToClientId = $reader->expand()->textContent;
+                $catchId = true;
+            }
+
+            $isLicTag = in_array(
+                $name,
+                ['ns1:FNSLicUlResponse', 'ns1:FNSLicIPResponse'],
+                true
+            );
+            if ($isPrimary && $isLicTag && $isElement) {
+                $isLicense = true;
+
+                $errorCode = (int)$reader->getAttribute('КодОбр');
+                $errorText = $reader->getAttribute('Ошибка');
+            }
+            if ($isPrimary
+                && $name === 'tns:ZPVIPEGRResponse'
+                && $isElement) {
+                $isFile = true;
+            }
+            if ($isFile && $name === 'fnst:ИмяФайла' && $isElement) {
+                $filename = $reader->expand()->textContent;
+                $catchFilename = true;
+            }
+            if ($isFile && $name === 'fnst:ТипФайла' && $isElement) {
+                $fileType = $reader->expand()->textContent;
+                $catchFileType = true;
+            }
+            if ($isFile && $name === 'fnst:ИмяАрхива' && $isElement) {
+                $archiveName = $reader->expand()->textContent;
+                $catchArchiveName = true;
+            }
         }
 
-        $name = $reader->name;
-        if ($name === 'messageType'
-            && $reader->nodeType === XMLReader::ELEMENT) {
-            $messageType = $reader->expand()->textContent;
-            $isStatus = $messageType === 'StatusMessage';
-            $isPrimary = $messageType === 'PrimaryMessage';
+        if ($catchId && $defineIdentity !== false) {
+            $uid = $record['uid'];
+            $defineIdentity->execute();
+        }
+        $letDefineAsReject = true;
+        if ($isStatus
+            && $errorCode === 0
+        ) {
+            $letDefineAsReject = !is_numeric($statusCode);
+        }
+        if ($letDefineAsReject) {
+            $errorCode = CODE_403_REJECT;
+        }
+        if ($isStatus && $catchId && $setStatus !== false) {
+            $setStatus->execute();
+        }
+        if ($isFile && $catchMessageId && $writeAttachment !== false) {
+            $detailJson = json_encode(
+                [
+                    'path' => "in/$messageId/$archiveName",
+                    'filename' => $filename,
+                    'fileType' => $fileType,
+                    'archiveName' => $archiveName,
+                ]
+            );
+            $uid = $record['uid'];
+            $writeAttachment->execute();
+        }
+        if ($isLicense && $catchId && $writeLicense !== false) {
+            $writeLicense->execute();
+        }
 
-            continue;
-        }
-        if ($name === 'replyToClientId'
-            && $reader->nodeType === XMLReader::ELEMENT) {
-            $replyToClientId = $reader->expand()->textContent;
-            $catchId = true;
-
-            continue;
-        }
-
-        if ($isPrimary
-            && $name === 'ns1:FNSLicUlResponse'
-            && $reader->nodeType === XMLReader::ELEMENT) {
-            $isLicense = true;
-
-            $errorCode = $reader->getAttribute('КодОбр');
-            $errorText = $reader->getAttribute('Ошибка');
-        }
-        if ($isPrimary
-            && $name === 'tns:ZPVIPEGRResponse'
-            && $reader->nodeType === XMLReader::ELEMENT) {
-            $isFile = true;
-        }
-        if ($isFile
-            && $name === 'fnst:ИмяФайла'
-            && $reader->nodeType === XMLReader::ELEMENT) {
-            $filename = $reader->expand()->textContent;
-        }
-        if ($isFile
-            && $name === 'fnst:ТипФайла'
-            && $reader->nodeType === XMLReader::ELEMENT) {
-            $fileType = $reader->expand()->textContent;
-        }
-        if ($isFile
-            && $name === 'fnst:ИмяАрхива'
-            && $reader->nodeType === XMLReader::ELEMENT) {
-            $archiveName = $reader->expand()->textContent;
-        }
-    }
-
-    if (
-        $catchId
-        && $defineIdentity !== false
-    ) {
-        $uid = $record['uid'];
-        $defineIdentity->execute();
-    }
-    if (
-        $catchId
-        && $isStatus
-        && $setStatus !== false
-    ) {
-        $errorCode = $record['error_code'];
-        $errorText = $record['error_text'];
-        $setStatus->execute();
-    }
-    if (
-        $catchId
-        && $isFile
-        && $writeAttachment !== false
-    ) {
-        $detailJson = json_encode(
+        $resultCode = CODE_200_OK;
+        $resultMessage = MESSAGE_200_OK;
+    } catch (Throwable $e) {
+        $resultCode = CODE_500_FAIL;
+        $resultMessage = json_encode(
             [
-                'filename' => $filename,
-                'fileType' => $fileType,
-                'archiveName' => $archiveName,
+                'Message' => $e->getMessage(),
+                'Code' => $e->getCode(),
+                'Trace' => $e->getTrace(),
             ]
         );
-        $writeAttachment->execute();
-    }
-    if (
-        $catchId
-        && $isLicense
-        && $writeLicense !== false
-    ) {
-        $writeLicense->execute();
     }
 
-    if ($markAsProcessed !== false) {
-        $uid = $record['uid'];
-        $markAsProcessed->execute();
-    }
+    $uid = $record['uid'];
+    $markAsProcessed->execute();
 }
 
 $db->commit();
